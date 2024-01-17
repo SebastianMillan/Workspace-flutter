b982f58cd7f5447f6c5fde9fd5ff8e5738f005d9
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-17 14:14:50 +0100

    Empezar ejercicio 7

---
+++ ejercicio7/lib/card_pokemon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardPokemon extends StatelessWidget {
+  const CardPokemon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
