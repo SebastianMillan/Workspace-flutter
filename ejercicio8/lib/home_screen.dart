046ba82c7fea3e82c3e3db794c29766cc76b1f1b
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-19 11:58:40 +0100

    Ejercicio 8 terminado

--- ejercicio8/lib/home_screen.dart
+++ ejercicio8/lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:ejercicio8/card_pokemon.dart';
 import 'package:ejercicio8/pokemon_response/pokemon_response.dart';
+import 'package:ejercicio8/pokemon_response/result.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,21 +17,21 @@ class _MyWidgetState extends State<HomeScreen> {
   Future<PokemonResponse> pokemonFuture = getPokemonList();
 
   static Future<PokemonResponse> getPokemonList() async {
-    var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/?limit=200');
+    var url = Uri.parse('https://pokeapi.co/api/v2/pokemon/?limit=50');
     final response =
-        await http.get(url, headers: {"Content-Type": "application/json"});
+        await http.get(url, headers: {'Accept': 'application/json'});
     if (response.statusCode == 200) {
-      return json.decode(response.body);
+      return PokemonResponse.fromJson(json.decode(response.body));
     } else {
       throw Exception('Unexpected error occured!');
     }
   }
 
-  Widget buildPoke(PokemonResponse pokemons) {
+  Widget buildPoke(List<Result> pokemons) {
     // ListView Builder to show data in a list
     return ListView.builder(
       itemBuilder: (context, index) {
-        final pokemon = pokemons.results![index];
+        final pokemon = pokemons[index];
         return CardPokemon(pokemon: pokemon);
       },
     );
@@ -40,19 +41,15 @@ class _MyWidgetState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        // FutureBuilder
         child: FutureBuilder<PokemonResponse>(
           future: getPokemonList(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              // until data is fetched, show loader
               return const CircularProgressIndicator();
             } else if (snapshot.hasData) {
-              // once data is fetched, display it on screen (call buildPosts())
-              final poke = snapshot.data!;
+              final poke = snapshot.data!.results!;
               return buildPoke(poke);
             } else {
-              // if no data, show simple Text
               return const Text("No data available");
             }
           },
